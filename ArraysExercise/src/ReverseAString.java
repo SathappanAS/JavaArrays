bfedc85dc33d0a114a11a610932cffe0f0c41b77
sathappan1989 <sathappanas@outlook.com>
2025-04-23 22:10:54 -0700

    ReverseAString

--- ArraysExercise/src/ReverseAString.java
+++ ArraysExercise/src/ReverseAString.java
@@ -11,15 +11,29 @@ public class ReverseAString {
 		System.out.println(Arrays.toString(s));
 		System.out.println(s.length);
 		int len = s.length; //5
+		/*char with new array*/
+//		char temp[]=new char[len];
+//		int j=0;
+//		for( int i=len-1;i>=0;i--) {
+//	
+//			temp[j]=s[i];
+//			j++;
+//		}
+//		System.out.println(Arrays.toString(temp));
 		
-		char temp[]=new char[len];
-		int j=0;
-		for( int i=len-1;i>=0;i--) {
-	
-			temp[j]=s[i];
-			j++;
+		
+		/*Reverse an array with same char*/
+		char a=' ';
+		for(int i=0,j=len-1;i<len/2 && j>len/2;i++,j--) {
+		
+		
+			a=s[i];
+			s[i]=s[j];
+			s[j]=a;
+
 		}
-		System.out.println(Arrays.toString(temp));
+		System.out.println(Arrays.toString(s));
+		
 	}
 
 }
